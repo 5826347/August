998018be61a04173473f926ed2d132e9850eaf1c
5826347 <2385783269@qq.com>
2023-08-01 13:25:25 +0800

    更新

--- src/One/there.java
+++ src/One/there.java
@@ -2,4 +2,8 @@ package One;
 
 public class there {
     //三
+    public static void main(String[] args) {
+        two two=new two();
+        two.aca();
+    }
 }
--- src/One/two.java
+++ src/One/two.java
@@ -1,6 +1,20 @@
 package One;
 
+import java.io.Writer;
+
+
+/**
+ * @author: mario
+ * @date: 19-07-08
+ * @version: 1.0.0
+ * @deion: 生成Jpg工具类
+ */
+
 public class two {
+
+    public void two() {
+
+    }
 
     static int a;
     //二
@@ -8,6 +22,26 @@ public class two {
 
     public static void main(String[] args) {
 
-        System.out.println(a);
+        long aaa = 2147483647;
+        while (true) {
+
+            if (aaa != 0) {
+                aaa += 1;
+                System.out.println(aaa);
+            } else
+                break;
+        }
+
     }
+
+
+    /**
+     * @author: mario
+     * @date: 19-07-08
+     * @version: 1.0.0
+     * @deion: 生成Jpg工具类
+     */
+    public void aca() {
+    }
+
 }
